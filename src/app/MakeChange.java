4429027517988a4894b1712ffb80ca2bcee062b0
Tom <tom.mcglone1@yahoo.com>
2023-01-14 09:12:14 -0500

    First commit before figuring out String change

--- src/app/MakeChange.java
+++ src/app/MakeChange.java
@@ -1,10 +1,41 @@
 package app;
-
+import java.util.Scanner;
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner (System.in);
+		
+		double itemPrice;
+		double tendered;
+		double change;
+		
+		System.out.print("Enter the price of the item to be purchased: ");
+		itemPrice = sc.nextDouble();
+		sc.nextLine();
+		
+		System.out.print("Enter the amount tendered by the customer: ");
+		tendered = sc.nextDouble();
+		sc.nextLine();
+		
+			if (itemPrice > tendered) {
+				System.err.println("Amount tendered does not cover cost of item." +
+									" Please provide sufficient amount.");
+			}
+			
+			else if (itemPrice % tendered == 0) {
+				System.out.println("Exact amount tendered. No change required.");
+			}
+			
+			else if (itemPrice < tendered) {
+				change = tendered - itemPrice;
+				
+				System.out.println(change);
+			}
+			
+		
 	}
 
 }
+// itemPrice = 15 tendered = 20 result is 5 change.
+// price 13.10 tendered 14 3 quarters 1 dime 1 nickel.
+// price 6.37 tendered 10 3 one dollar bills, 3 pennies,  2 quarters one dime 
